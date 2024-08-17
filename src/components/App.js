7b5ae7ba6c2bb6858ed48277188fc043c2f11e9c
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-08-17 00:23:48 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 import TestComponent from "./TestComponent";
 
-// Add your code own within the return statement
 function App() {
   return (
     <div className="App">
